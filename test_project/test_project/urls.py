f857adcb7c84d2321a7040e7aef324020a826907
jpic <jamespic@gmail.com>
2012-10-22 10:42:08 +0200

    Enable installation for non authenticated users.

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -18,4 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^appstore/', include('appstore.urls')),
+    url(r'^auth/', include('django.contrib.auth.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
