95de575020be837cfdcfc1e89b199e68cadc0f92
jpic <jamespic@gmail.com>
2012-12-03 17:47:46 +0100

    Implemented post_app_copy for AppForm

--- appstore/contrib/form_designer_appeditor/models.py
+++ appstore/contrib/form_designer_appeditor/models.py
@@ -3,6 +3,7 @@ from django.db.models import signals
 
 from form_designer.models import Form
 from appstore.models import App
+from appstore.signals import post_app_copy
 
 
 class AppForm(models.Model):
@@ -23,3 +24,20 @@ def auto_appform(sender, instance, created, **kwargs):
     form = Form.objects.create(author_id=1, verbose_name=instance.name)
     AppForm(app=instance, form=form).save()
 signals.post_save.connect(auto_appform, sender=App)
+
+
+def copy_form(sender, source_app, new_app, **kwargs):
+    new_app.appform.form.tab_set.all().delete()
+
+    for tab in source_app.appform.form.tab_set.all():
+        new_tab = new_app.appform.form.tab_set.create(
+            name=tab.name,
+            verbose_name=tab.verbose_name,
+            order=tab.order)
+
+        for widget in tab.widget_set.all().select_subclasses():
+            widget.pk = None
+            widget.id = None
+            widget.tab = new_tab
+            widget.save()
+post_app_copy.connect(copy_form)
